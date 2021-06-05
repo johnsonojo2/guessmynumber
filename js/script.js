9a46c9eafc5615ccc8a09f21b8df8bf5aea14869
Johnson <johnsonojo2@gmail.com>
2021-06-05 13:55:05 +0100

    refactored the code to make it shorter nad more readable

--- js/script.js
+++ js/script.js
@@ -8,19 +8,33 @@ console.log(document.querySelector('.message').textContent);
 let sNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+
+// defining variables for the classes to be selected
+
+const messageClass = document.querySelector('.message');
+const numberClass = document.querySelector('.number');
+const guessClass = document.querySelector('.guess');
+const scoreClass = document.querySelector('.score');
+const bodyClass = document.querySelector('.bods');
+const headerClass = document.querySelector('header');
+const mainClass = document.querySelector('main');
+const checkClass = document.querySelector('.check');
+const btnClass = document.querySelector('.btn');
+const highScoreClass = document.querySelector('.highscore');
+
+// this is the function for display the state of the answer in the guess box
 const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
+  messageClass.textContent = message;
 };
 const displaysNumber = function (Numbe) {
-  document.querySelector('.number').textContent = Numbe;
+  numberClass.textContent = Numbe;
 };
-// const styleSelector = function (cName, property, value) {
-//   document.querySelector(cName).style.property = value;
-// };
+bodyClass.classList.remove('bod');
+numberClass.classList.remove('numberr');
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  // When there is no nput
+const playGame = function () {
+  const guess = Number(guessClass.value);
+  // When there is no input
   if (!guess) {
     displayMessage('😡 No number !');
     // when guess is wrong
@@ -28,51 +42,47 @@ document.querySelector('.check').addEventListener('click', function () {
     if (score > 1) {
       displayMessage(guess > sNumber ? 'Guess too high' : 'Guess too low');
       score--;
-      document.querySelector('.score').textContent = score;
+      scoreClass.textContent = score;
     } else {
       displayMessage('Sorry You lost the Game!');
-      document.querySelector('.score').textContent = 0;
+      scoreClass.textContent = 0;
     }
 
     // when guess is correct
   } else if (guess === sNumber) {
     displayMessage('Correct Number!');
     displaysNumber(sNumber);
-    document.querySelector('body').style.background = '#60b347';
-    document.querySelector('body').style.color = '#eee';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').style.background = '#eee';
-    document.querySelector('.number').style.color = '#333';
-    document.querySelector('header').style.borderBottomColor = '#eee';
-    document.querySelector('main').style.color = '#eee';
-    document.querySelector('.guess').style.borderColor = '#eee';
-    document.querySelector('.btn').style.background = '#eee';
-    document.querySelector('.check').style.background = '#eee';
+    bodyClass.classList.add('bod');
+    numberClass.classList.add('numberr');
+    headerClass.style.borderBottomColor = '#eee';
+    mainClass.style.color = '#eee';
+    guessClass.style.borderColor = '#eee';
+    btnClass.style.background = '#eee';
+    checkClass.style.background = '#eee';
 
     if (score > highScore) {
       highScore = score;
-      document.querySelector('.highscore').textContent = highScore;
+      highScoreClass.textContent = highScore;
     }
   }
-});
+};
+// This code is for playing again without refreshing the page
 
-document.querySelector('.again').addEventListener('click', function () {
+const playAgain = function () {
   score = 20;
   sNumber = Math.trunc(Math.random() * 20) + 1;
   displayMessage('Start guessing...');
-  document.querySelector('.score').textContent = score;
+  scoreClass.textContent = score;
   displaysNumber('?');
-  document.querySelector('.guess').value = '';
+  guessClass.value = '';
+  bodyClass.classList.remove('bod');
+  numberClass.classList.remove('numberr');
+  headerClass.style.borderBottomColor = '#ffc947';
+  mainClass.style.color = '#feddbe';
+  guessClass.style.borderColor = '#ffc947';
+  btnClass.style.background = '#ffc947';
+  checkClass.style.background = '#ffc947';
+};
 
-  document.querySelector('body').style.background = '#0a1931';
-  document.querySelector('body').style.color = '#feddbe';
-
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.number').style.background = '#ffc947';
-  document.querySelector('.number').style.color = '#0a1931';
-  document.querySelector('header').style.borderBottomColor = '#ffc947';
-  document.querySelector('main').style.color = '#feddbe';
-  document.querySelector('.guess').style.borderColor = '#ffc947';
-  document.querySelector('.btn').style.background = '#ffc947';
-  document.querySelector('.check').style.background = '#ffc947';
-});
+checkClass.addEventListener('click', playGame);
+document.querySelector('.again').addEventListener('click', playAgain);
